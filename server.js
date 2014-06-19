f85259b47b92cdd6a8228c67ea60530fcfad5c0b
Thomas Davis <thomasalwyndavis@gmail.com>
2014-06-20 01:24:18 +1000

    fixed https links

--- server.js
+++ server.js
@@ -59,7 +59,6 @@ function S4() {
 }
 MongoClient.connect(process.env.MONGOHQ_URL, function(err, db) {
   app.get('/', function(req, res) {
-    console.log('headers',req.headers);
     res.send(generatePage({
       page: {
         template: templates.home
