539193e7cec229ea45cd2c422e16902a3025adb7
Thomas Davis <thomasalwyndavis@gmail.com>
2014-06-19 20:35:53 +1000

    fix

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var bodyParser = require('body-parser');
 
 
 var app = express();
-app.use(express.static(__dirname + '/public', {maxAge: 7200 * 1000}));
+app.use(express.static(__dirname + '/public', {maxAge: 0 * 1000}));
 app.use(bodyParser());
 
 
